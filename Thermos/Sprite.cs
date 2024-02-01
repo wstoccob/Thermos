8bf776b8229f77cf0747107bf4085da05293dbfc
wstoccob <alinur.seysekov@gmail.com>
2024-02-01 22:45:51 +0600

    Add Sprite class

---
+++ Thermos/Sprite.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Thermos;
+
+public class Sprite
+{
+    private Texture2D _texture;
+    private Vector2 Position { get; set; }
+
+    public Sprite( Texture2D texture, Vector2 position)
+    {
+        _texture = texture;
+        Position = position;
+    }
+
+    public void Render(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, Position, Color.White);
+    }
+}
